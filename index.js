5d5e42039b8be699fe8db31ca5b8c20a03738bff
Paul Frazee <pfrazee@gmail.com>
2015-06-11 12:42:27 -0500

    add two weighted node-selection algs

--- index.js
+++ index.js
@@ -3,37 +3,16 @@ var graphmitter = require('graphmitter')
 // n, n^2 - 100 nodes, 200 edges is 2% the edges needed for full direct connectivity
 var N = 100
 var E = 200
-
-// find a random graph with full transitive connectivity
-var graph, reachable
-do { graph = graphmitter.random(N, E) }
-while (Object.keys(reachable = graph.traverse({ start: '#0', hops: 10 })).length !== 100)
-
-// output graph description
-var str = '', lastN = 0, reachableNum = 0
-for (var k in reachable) {
-  if (lastN < reachable[k]) {
-    if (reachableNum)
-      str += '\n'+reachable[k]+' hops: ' + reachableNum
-    lastN = reachable[k]
-    reachableNum = 0
-  }
-  reachableNum++
-}
-if (reachableNum)
-  str += '\n'+(lastN+1)+' hops: ' + reachableNum
-console.log('Graph generated, connectivity from node #0...', str)
-
-// run simulations
-console.log('\nSIM RESULTS')
-console.log('A datum was gossiped across the network, starting from node #0, using...')
-console.log('Random selection from all nodes:    ', runSimulation(randomNode), 'rounds')
-console.log('Random selection from nodes\' edges: ', runSimulation(randomFromEdges), 'rounds')
-
+var graph
+var numInbounds = {} // number of inbound links for each node
 
 // helpers
+// =======
 
 var simnum = 1
+var nodesArray = []
+for (var i=0; i < N; i++)
+  nodesArray.push('#'+i)
 
 function fullyDispersed () {
   for (var i=0; i < N; i++) {
@@ -50,6 +29,47 @@ function randomNode () {
 function randomFromEdges (node) {
   var edgeKeys = Object.keys(node.edges)
   return edgeKeys[Math.floor(Math.random() * edgeKeys.length)]
+}
+
+function randomWeightedByFailures (node) {
+  node.failures = node.failures || {}
+  
+  // create a set of options
+  var options = nodesArray.filter(function (peer) {
+    var failures = node.failures[peer] || 0
+    var lim = (1/(1+failures)) || 1 // decrease odds of selection due to failures
+    return (Math.random() < lim)
+  })
+
+  // pick from our options
+  var peer = options[Math.floor(Math.random() * options.length)]
+  
+  // will this fail? track the failure
+  if (!node.edges[peer])
+    node.failures[peer] = (node.failures[peer]||0) + 1
+
+  return peer
+}
+
+function randomWeightedByPopularityAndFailures (node) {
+  node.failures = node.failures || {}
+  
+  // create a set of options
+  var options = nodesArray.filter(function (peer) {
+    var popularity = Math.min(numInbounds[peer] || 0, 10)
+    var failures = node.failures[peer] || 0
+    var lim = (popularity+10)/((failures+1)*20) // decrease odds of selection due to failures
+    return (Math.random() < lim)
+  })
+
+  // pick from our options
+  var peer = options[Math.floor(Math.random() * options.length)]
+  
+  // will this fail? track the failure
+  if (!node.edges[peer])
+    node.failures[peer] = (node.failures[peer]||0) + 1
+
+  return peer  
 }
 
 function runSimulation (pickPeer) {
@@ -82,3 +102,41 @@ function runSimulation (pickPeer) {
   simnum++
   return iterations
 }
+
+// simulation
+// ==========
+
+// find a random graph with full transitive connectivity
+var reachable
+do { graph = graphmitter.random(N, E) }
+while (Object.keys(reachable = graph.traverse({ start: '#0', hops: 10 })).length !== 100)
+
+// count the inbounds
+graph.each(function (k, node) {
+  for (var e in node.edges) {
+    numInbounds[e] = (numInbounds[e]||0) + 1
+  }
+})
+
+// output graph description
+var str = '', lastN = 0, reachableNum = 0
+for (var k in reachable) {
+  if (lastN < reachable[k]) {
+    if (reachableNum)
+      str += '\n'+reachable[k]+' hops: ' + reachableNum
+    lastN = reachable[k]
+    reachableNum = 0
+  }
+  reachableNum++
+}
+if (reachableNum)
+  str += '\n'+(lastN+1)+' hops: ' + reachableNum
+console.log('Graph generated, connectivity from node #0...', str)
+
+// run simulations
+console.log('\nSIM RESULTS')
+console.log('A datum was gossiped across the network, starting from node #0, using...')
+console.log('Random selection from all nodes:', runSimulation(randomNode), 'rounds')
+console.log('Random selection from nodes\' edges:', runSimulation(randomFromEdges), 'rounds')
+console.log('Random selection from all nodes, weighted by past failures:', runSimulation(randomWeightedByFailures), 'rounds')
+console.log('Random selection from all nodes, weighted by popularity and past failures:', runSimulation(randomWeightedByPopularityAndFailures), 'rounds')
